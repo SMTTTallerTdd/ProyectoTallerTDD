843a2c0e8d41234bc02014611c60df8704d45cee
gsequera <gelvis.sequera@smartmatic.com>
2013-12-14 11:34:31 -0500

    Primeta historia y saltos.

---
+++ src/main/java/com/smartmatic/tdd/word/WordUtils.java
@@ -0,0 +1,13 @@
+package com.smartmatic.tdd.word;
+
+public class WordUtils {
+
+	public WordUtils() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static boolean isValidWord(String word) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
--- src/main/java/controller/AppController.java
+++ src/main/java/controller/AppController.java
@@ -2,12 +2,18 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.smartmatic.tdd.word.WordUtils;
 
 @Controller
 public class AppController {
 
 	@RequestMapping("/")
-	public String home(){
-		return "home";
+	public String home(@RequestParam String name){
+		if(!WordUtils.isValidWord(name))
+			return "home";
+		else
+			return "game";
 	}
 }
